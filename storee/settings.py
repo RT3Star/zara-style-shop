8735fef6a646c58302b37da96d256ec75ea0f33e
RT3Star <romanstaroselets2011@gmail.com>
2026-06-03 00:59:58 +0300

    Fix: Use correct Redis host for Railway

--- storee/settings.py
+++ storee/settings.py
@@ -106,14 +106,9 @@ DATABASES["default"]["OPTIONS"] = {
 # Встановіть бібліотеку: pip install django-redis
 
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-        "KEY_PREFIX": "zara_shop",
-        "TIMEOUT": 300,  # 5 хвилин
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/1',
     }
 }
 
