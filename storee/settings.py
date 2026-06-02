3fc0cda2650a8544ab874ffc626c52c04384f31b
RT3Star <romanstaroselets2011@gmail.com>
2026-06-03 00:34:19 +0300

    Fix ALLOWED_HOSTS for Railway

--- storee/settings.py
+++ storee/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY", default="django-insecure-default-key-for-dev")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
