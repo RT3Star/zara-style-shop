7e1fdcf605dae3d1eec9870a9593f896dd19e2bf
RT3Star <romanstaroselets2011@gmail.com>
2026-06-03 00:39:54 +0300

    FIX two part

--- storee/settings.py
+++ storee/settings.py
@@ -25,7 +25,7 @@ import os
 SECRET_KEY = config("SECRET_KEY", default="django-insecure-default-key-for-dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
